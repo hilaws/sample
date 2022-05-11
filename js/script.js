ff92acaad68075dc3224bc12f4b098e6138fa06b
Muhammed swalih <Mswalih208@gmail.com>
2022-05-11 14:13:22 +0530

    secound-day

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+    $('#slider').nivoSlider({
+        effect: 'random',
+        directionNavHide: false,
+        pauseOnHover: true,
+        captionOpacity: 1,
+        prevText: '<',
+        nextText: '>'
+    });
+});
