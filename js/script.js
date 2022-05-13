9f7e1c71761edf534e0023fc220381d7cbf130de
Muhammed swalih <Mswalih208@gmail.com>
2022-05-13 14:48:50 +0530

    refactor:web

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-$(window).load(function() {
-    $('#slider').nivoSlider({
-        effect: 'random',
-        directionNavHide: false,
-        pauseOnHover: true,
-        captionOpacity: 1,
-        prevText: '<',
-        nextText: '>'
-    });
-});
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
